4d4bbe9130fba9c0c07beea9d28f47ecc77a42df
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-08-30 22:21:15 -0500

    Add Validate Command

--- src/commonMain/kotlin/xyz.calcugames.levelz.cli/main.kt
+++ src/commonMain/kotlin/xyz.calcugames.levelz.cli/main.kt
@@ -1,10 +1,14 @@
 package xyz.calcugames.levelz.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 
 class LevelZ : CliktCommand(name = "levelz") {
     override fun run() = Unit
 }
 
 fun main(args: Array<String>) = LevelZ()
+    .subcommands(
+        Validate()
+    )
     .main(args)
